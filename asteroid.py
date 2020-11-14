dd727e466d1f0235717c04445bc749f7d1f1044b
Gauthier I <gauthier.imbert@insa-rouen.fr>
2020-11-14 18:12:42 +0100

    Ajout docstring module asteroid.py

--- asteroid.py
+++ asteroid.py
@@ -1,9 +1,20 @@
-import pygame
+"""
+Le module asteroid permet de représenter un astéroïde.
+"""
+
 import random
+import pygame
+
 
 class Asteroid(pygame.sprite.Sprite):
+    """
+    La classe Asteroid permet de représenter un astéroïde.
+    """
 
     def __init__(self, game):
+        """
+        Constructeur de la classe
+        """
         super().__init__()
         self.game = game
         self.atk = 1
@@ -13,8 +24,10 @@ class Asteroid(pygame.sprite.Sprite):
         self.rect.y = random.randint(0, 380 - self.rect.width)
         self.velocity = random.randint(1, 10)
 
-
     def avancer(self):
+        """
+        Méthode permettant à l'astéroïde d'avancer dans la fenêtre.
+        """
         if not self.rect.x < -self.rect.width:
             if self.game.detect_collision(self, self.game.all_player):
                 self.game.player.damage(self.atk)
@@ -24,8 +37,10 @@ class Asteroid(pygame.sprite.Sprite):
             self.reaparition()
 
     def reaparition(self):
+        """
+        Méthode gérant la réaparition de l'astéroïde
+        (permet d'éviter la création de multiples objets).
+        """
         self.rect.x = 697 + random.randint(0, 300)
         self.rect.y = random.randint(0, 380 - self.rect.width)
         self.velocity = random.randint(1, 3)
-
-
